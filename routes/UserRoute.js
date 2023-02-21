b70301baa997cbcd15004ac93d91ca815cc297bb
salmancheema05 <salmancheematnt12@gmail.com>
2023-02-21 03:22:59 -0800

    update backend

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -3,4 +3,7 @@ import { usersignup, userlogin  } from "../controllers/UserController.js";
 const router = express.Router()
 router.post('/api/usersignup',usersignup)
 router.post('/api/userlogin',userlogin)
+router.get('/',(req,res)=>{
+    res.send('welcome to node js')
+})
 export default router
