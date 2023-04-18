fc7c885e5f3fefc50afa76693f78e5492b46d036
salmancheema05 <salmancheematnt12@gmail.com>
2023-04-18 12:30:01 -0700

    package json update

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,5 +4,8 @@ const router = express.Router()
 router.post('/api/usersignup',userSignup)
 router.post('/api/userlogin',userLogin )
 router.post('/api/userlogout',userLogout )
+router.get('/',(req,res) =>{
+    res.send('node start')
+})
 
 export default router
